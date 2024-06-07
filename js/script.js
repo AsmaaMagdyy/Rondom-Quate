84d58983b8445cca2e81d623b5d32c59fb6ae287
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2024-06-07 20:11:46 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -7,12 +7,18 @@ var qoutes = [`"Be yourself; everyone else is already taken"`,
 
 var num ;
 var last;
+var alreadyExist =[];
 function generateRondomNum(){
+  if(qoutes.length==alreadyExist.length){
+    alreadyExist=[];
+  }
   do{
     num = Math.floor(Math.random() * qoutes.length);
-  }while(last===num){
+  }while(last===num || alreadyExist.includes(num))
     last = num;
     document.getElementById('quote').innerHTML = qoutes[num];
-  }
+    alreadyExist.push(num);
+    console.log(num);
+  
 
 }
