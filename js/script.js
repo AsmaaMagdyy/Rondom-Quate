f6016e9989c26bb1f217cce318ee8d98002e2039
AsmaaMagdyy <asmaa.magdy8921@gmail.com>
2024-06-07 15:36:01 +0300

    sec commit

--- js/script.js
+++ js/script.js
@@ -1,29 +1,18 @@
 
 var qoutes = [`"Be yourself; everyone else is already taken"`,
-                `"So many books, so little time"`,
-                `"Be the change that you wish to see in the world"`,
-                `"A room without books is like a body without a soul"`,
-                `"You only live once, but if you do it right, once is enough"`];
+  `"So many books, so little time"`,
+  `"Be the change that you wish to see in the world"`,
+  `"A room without books is like a body without a soul"`,
+  `"You only live once, but if you do it right, once is enough"`];
 
-            var num=Math.floor(Math.random()*qoutes.length);
-            var last=num;
-            function generateRondomNum(){
-          
-            num=Math.floor(Math.random()*qoutes.length);
-             if(num==last){
-              if(num==qoutes.length){
-                num--;
-                document.getElementById('quote').innerHTML=qoutes[num];
-                num=Math.floor(Math.random()*qoutes.length);
-                last=num;
-              }else{
-                num++;
-                document.getElementById('quote').innerHTML=qoutes[num];
-                num=Math.floor(Math.random()*qoutes.length);
-                last=num;
-              }
-             }else{
-                document.getElementById('quote').innerHTML=qoutes[num];
-                last=num;
-             }
-            }
+var num ;
+var last;
+function generateRondomNum(){
+  do{
+    num = Math.floor(Math.random() * qoutes.length);
+  }while(last===num){
+    last = num;
+    document.getElementById('quote').innerHTML = qoutes[num];
+  }
+
+}
